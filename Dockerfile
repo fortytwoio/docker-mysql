94c84d607929abc17b457519c0f6785863a48033
Thomas Fritz <fritztho@gmail.com>
2014-09-02 20:51:34 +0200

    Updated my.cnf. Moved my.cnf to etc/mysql. Added fig.yml

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,14 @@
-## Version 1.0.2
-FROM fortytwoio/base-image:latest
+## Version 1.0.3
+FROM fortytwoio/base:latest
 MAINTAINER Thomas Fritz <thomas.fritz@forty-two.io>
 
-ADD http://repo.mysql.com/mysql-apt-config_0.2.1-1debian7_all.deb /tmp/
-RUN DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/mysql-apt-config_0.2.1-1debian7_all.deb
+ADD http://repo.mysql.com/mysql-apt-config_0.2.1-1debian7_all.deb /tmp/mysql-apt-config.deb
+RUN DEBIAN_FRONTEND=noninteractive dpkg -i /tmp/mysql-apt-config.deb
 
 RUN	DEBIAN_FRONTEND=noninteractive apt-get update -qqy > /dev/null 2>&1 && \
     DEBIAN_FRONTEND=noninteractive apt-get install -qqy --no-install-recommends perl mysql-server > /dev/null 2>&1
 
-ADD ./run.sh /root/run.sh
-RUN chmod +x /root/run.sh
-
-ADD ./my.cnf /root/my.cnf
+ADD ./etc/mysql/my.cnf /etc/mysql/my.cnf.tpl
 
 EXPOSE 3306
 VOLUME ["/var/lib/mysql"]
-
-ENTRYPOINT ["/root/run.sh"]
-
--- bin/start-mysql.sh
+++ bin/start-mysql.sh
@@ -3,16 +3,12 @@ set -eu # exit on error or undefined variable
 
 # Defaults
 export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-""}
-
 export MYSQL_USER=${MYSQL_USER:-""}
 export MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 export MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-
 export MYSQL_RAMDISK=${MYSQL_RAMDISK:-""}
-
 export MYSQL_SET_KEYBUF=${MYSQL_SET_KEYBUF:-"128M"}
 export MYSQL_SET_QCACHE=${MYSQL_SET_QCACHE:-"32M"}
-
 
 
 # Ramdisk?
@@ -23,17 +19,17 @@ if [[ ! -z $MYSQL_RAMDISK ]]; then
 fi
 
 
-
 # Permissions
 chown -R mysql:mysql /var/lib/mysql
 chmod -R ug+rwX,o-rwx /var/lib/mysql
+mkdir -p /var/log/mysql
+chown -R mysql:mysql /var/log/mysql
 
 # Config file template from environment variables
-envsubst < /root/my.cnf > /etc/mysql/my.cnf
+envsubst < /etc/mysql/my.cnf.tpl > /etc/mysql/my.cnf
 
 # Initialize MySQL data directory
 mysql_install_db --user mysql $@ > /dev/null
-
 
 
 # Execute SQL commands
@@ -48,7 +44,6 @@ function mysql_init_commands(){
 }
 
 mysql_init_commands | /usr/sbin/mysqld --user=mysql --bootstrap --verbose=0 $@
-
 
 
 # Launch
